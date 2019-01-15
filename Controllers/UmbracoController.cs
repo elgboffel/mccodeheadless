89e94a544ebe00b1345c84b06d238caba77c7a89
elgboffel <johnny2900@gmail.com>
2019-01-15 21:55:42 +0100

    First comit

---
+++ Controllers/UmbracoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Umbraco.Headless.Client.Net.Services;
+using Umbraco.Headless.Client.Net.Web;
+
+namespace McCode.Controllers
+{
+    public class UmbracoController : DefaultUmbracoController
+    {
+        public UmbracoController(UmbracoContext umbracoContext, PublishedContentService headlessService) : base(umbracoContext, headlessService)
+        {
+        }
+
+        public override Task<IActionResult> Index()
+        {
+            // get the content for the current route
+            var content = UmbracoContext.GetContent();
+            // return the view which will be located at /Views/Page/Index.cshtml
+            return Task.FromResult((IActionResult)View(content));
+        }
+    }
+}
